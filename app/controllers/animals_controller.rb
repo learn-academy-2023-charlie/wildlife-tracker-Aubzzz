907bac4385abdd1cab95a7eb050cab970d00c533
Aubzzz <aubrey.diehl28@gmail.com>
2023-05-26 12:01:42 -0700

    more updates

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -8,4 +8,13 @@ def show
     animal = Animal.find(params[:id])
     render json: animal
 end
+
+def create
+    animal = Animal.create(animal_params)
+end
+
+
+private
+def animal_params
+    params.require(:animal).permit(:common_name, :scientific_binomial)
 end
